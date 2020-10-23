2911484eb7148fdfb1e134af815d3437efc9c30b
nadav pesach <nadav-pesach@users.noreply.github.com>
2020-10-23 18:36:03 +0300

    1.01
    update

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 import requests
 
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=["GET", "POST"])
 def index():
     return render_template('index.html')
 
-@app.route('/search')
+
+@app.route('/search', methods=["GET", "POST"])
 def search():
-    # item = request.form.get('mybooks')
-    # resulte = requests.get(f'')
-    return render_template('search.html')
+    items =  request.args.get('mysearch')
+    options = request.args.get('cle')
+    resulte = 'wrong input, try to search again'
+    resp = requests.get(f'https://rickandmortyapi.com/api/{options}').json()
+    if items:
+        for i in range(20):
+            if resp['results'][i]['name'].lower().startswith(items.lower()):
+                resulte = resp['results'][i]
+        return render_template('search.html', yoursearch=resulte)
+    else:
+        resulte = resp
+    return render_template('search.html', yoursearch=resulte)
+
 
 @app.route('/about-us')
 def about_us():
