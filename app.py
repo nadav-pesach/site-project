21df84f60cc6a3bd14e2bd0ebc99665e4568a7e9
nadav pesach <nadav-pesach@users.noreply.github.com>
2020-10-23 22:47:02 +0300

    1.011

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, url_for, request, redirect
 import requests
-
+from flask import Flask, redirect, render_template, request, url_for
+import random
 
 app = Flask(__name__)
 
@@ -16,14 +16,20 @@ def search():
     options = request.args.get('cle')
     resulte = 'wrong input, try to search again'
     resp = requests.get(f'https://rickandmortyapi.com/api/{options}').json()
-    if items:
-        for i in range(20):
-            if resp['results'][i]['name'].lower().startswith(items.lower()):
-                resulte = resp['results'][i]
-        return render_template('search.html', yoursearch=resulte)
-    else:
-        resulte = resp
-    return render_template('search.html', yoursearch=resulte)
+    if items and options == "character":
+        for i in range(35):
+            resp = requests.get(f'https://rickandmortyapi.com/api/{options}?page={i}').json()
+            for i in range(21):
+                try:
+                    if resp['results'][i]['name'].lower().startswith(items.lower()):
+                        resulte = resp['results'][i]
+                        return render_template('search.html', yoursearch=resulte, options=options, items=items)
+                except IndexError as err:
+                    print(err)
+    elif options == "character":
+        resulte = requests.get(f'https://rickandmortyapi.com/api/{options}?page={random.randint(0, 34)}').json()['results']
+        return render_template('search.html', yoursearch=resulte, options=options)
+    return render_template('search.html', yoursearch=resulte, options=options)
 
 
 @app.route('/about-us')
