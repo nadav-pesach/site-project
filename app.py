ed00edc2799b8d823b3dfe712f3fa173031d89a1
nadav pesach <nadav-pesach@users.noreply.github.com>
2020-10-24 13:52:53 +0300

    1.013

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 @app.route('/', methods=["GET", "POST"])
 def index():
     resulte = []
-    for i in range(3):
+    number = random.randint(0, 32)
+    for i in range(number, number + 3):
             resulte += requests.get(f'https://rickandmortyapi.com/api/character?page={i}').json()['results']
     return render_template('index.html', yoursearch=resulte)
 
@@ -18,31 +19,40 @@ def search():
     items =  request.args.get('mysearch')
     options = request.args.get('cle')
     resulte = []
-    if options:
-        # can remove option latter and then if items only and change option to https://rickandmortyapi.com/api/character
-        if items and options == "character":
-            resp = []
-            for i in range(35):
-                resp += requests.get(f'https://rickandmortyapi.com/api/{options}?page={i}').json()['results']
-            for i in resp:
-                try:
-                    if i['name'].lower().startswith(items.lower()):
-                        resulte += [i]
-                except IndexError as err:
-                    print(err)
-            if resulte:
-                return render_template('search.html', yoursearch=resulte, options=options, items=items)
-            else:
-                return render_template('search.html', yoursearch='wrong input, try to search again')
-        elif options == "character":
-            resulte = requests.get(f'https://rickandmortyapi.com/api/{options}?page={random.randint(0, 34)}').json()['results']
-            return render_template('search.html', yoursearch=resulte, options=options)
-        resulte = requests.get(f'https://rickandmortyapi.com/api/{options}').json()
+    if items and options == "character":
+        resp = []
+        for i in range(35):
+            resp += requests.get(f'https://rickandmortyapi.com/api/{options}?page={i}').json()['results']
+        for i in resp:
+            try:
+                if i['name'].lower().startswith(items.lower()):
+                    resulte += [i]
+            except IndexError as err:
+                print(err)
+        if resulte:
+            return render_template('search.html', yoursearch=resulte, options=options, items=items)
+        else:
+            return render_template('index.html')
+    elif options == "character":
+        resulte = requests.get(f'https://rickandmortyapi.com/api/{options}?page={random.randint(0, 34)}').json()['results']
         return render_template('search.html', yoursearch=resulte, options=options)
-    else:
-        name_id = request.args.get('pictursearch')
-        one = requests.get(f'https://rickandmortyapi.com/api/character/{name_id}').json()
-        return render_template('search.html', name_id=one)
+    elif options == "episode":
+        if not items:
+            items = random.randint(1, 41)
+        resp = requests.get(f'https://rickandmortyapi.com/api/episode/{items}').json()
+        if 'error' in resp:
+            return render_template('index.html')
+        episode = resp['episode']
+        name = resp['name']
+        air_date = resp['air_date']
+        resulte = []
+        for person in resp['characters']:
+            # resulte = person
+            resulte += [requests.get(person).json()]
+        return render_template('search.html', yoursearch=resulte, name=name, air_date=air_date, options=options, items=items, episode=episode)
+    name_id = request.args.get('pictursearch')
+    one = requests.get(f'https://rickandmortyapi.com/api/character/{name_id}').json()['characters']
+    return render_template('search.html', name_id=one)
 
 @app.route('/about-us')
 def about_us():
